9b8c7aa2b6da0f2e0a71f7bd497fdc23c3331913
pparfrey <44984488+pparfrey@users.noreply.github.com>
2021-02-20 20:15:20 -0500

    REFACTOR: Changed the way paths are handled, changed "type" -> "extension"

--- src/universal_devkit/prepare_data/extension_unifier.py
+++ src/universal_devkit/prepare_data/extension_unifier.py
@@ -1,20 +1,22 @@
 import argparse
 import os
+from shutil import copyfile
+from pathlib import Path
 
 # Change all file extensions in input_directory to a single extension
 # and output to output_directory
 
 def main(input_directory, output_directory, ext_type):
     if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+        Path.mkdir(output_directory, parents=False, exist_ok=False)
 
-    os.chdir(input_directory)
-    
+
     for filename in os.listdir(input_directory):
         base = os.path.splitext(filename)[0]
         new_name = base + ext_type
-        os.rename(filename, new_name)
-        os.rename(input_directory + '\\' + new_name, output_directory + '\\' + new_name)
+        input_path = os.path.join(input_directory, filename)
+        output_path = os.path.join(output_directory, new_name)
+        copyfile(input_path, output_path)
 
 
 if __name__ == "__main__":
@@ -24,7 +26,7 @@ if __name__ == "__main__":
 
     parser.add_argument("-i", "--input", type=str, help="The input directory")
 
-    parser.add_argument("-t", "--type", type=str, help="Extension you want the files to be")
+    parser.add_argument("-e", "--extension", type=str, help="Extension you want the files to be")
 
     args = vars(parser.parse_args())
-    main(args["input"], args["output"], args["type"])
+    main(args["input"], args["output"], args["extension"])
