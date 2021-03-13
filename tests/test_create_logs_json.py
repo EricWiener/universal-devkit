560f4a40ac6e960925c6a750c2f3dbd2b048d634
pparfrey <44984488+pparfrey@users.noreply.github.com>
2021-03-13 11:12:26 -0500

    TEST: Added test file to compare to directly

--- tests/test_create_logs_json.py
+++ tests/test_create_logs_json.py
@@ -1,49 +1,60 @@
 from universal_devkit.prepare_data.create_logs_json import get_logs
+import shutil
+from pathlib import Path
+from universal_devkit.utils import read_json
+
+def get_relative_path(relative_file_path):
+    return Path(__file__).parent / relative_file_path
+
+
+def equal_dicts(d1: dict, d2: dict, ignore_keys: list):
+    """Check two dictionaries for equality with the ability to ignore
+    specific keys.
+    Source: https://stackoverflow.com/a/10480904/6942666
+    Args:
+        d1 (dict): the first dictionary
+        d2 (dict): the second dictionary
+        ignore_keys (list): a list of strings with keys to ignore
+    Returns:
+        bool: whether the dicts are equal
+    """
+    d1_filtered = {k: v for k, v in d1.items() if k not in ignore_keys}
+    d2_filtered = {k: v for k, v in d2.items() if k not in ignore_keys}
+    return d1_filtered == d2_filtered
 
 def test_create_logs_json():
-    correct_output = []
+# The path to the folder with example logs
+    example_logs_path = get_relative_path(
+        "assets/example_logs"
+    )
 
-    # Row 1
-    temp_row = {"token": 0,
-                "logfile": "log1.txt",
-                "vehicle": "big boat",
-                "date_captured": "2021-07-01",
-                "location": "music pond"
-                }
-    correct_output.append(temp_row)
+    # The correct output for the example logs
+    correct_output_path = get_relative_path(
+        "assets/create_logs_json_correct.json"
+    )
 
-    # Row 2
-    temp_row = {"token": 0,
-                "logfile": "log2.txt",
-                "vehicle": "small boat",
-                "date_captured": "2021-02-01",
-                "location": "other pond"
-                }
-    correct_output.append(temp_row)
+    # The path to use as the output directory
+    output_file_dir = "universal_devkit/tests"
+    
+    # The file to be used as output
+    output_file_path = "universal_devkit/tests/example_logs_output.json" # What is file name going to be?
+    
+    # Convert the example logs
+    output = get_logs(example_logs_path)
 
-    # Row 3
-    temp_row = {"token": 0,
-                "logfile": "random-name.txt",
-                "vehicle": "medium boat",
-                "date_captured": "2020-07-01",
-                "location": "not a pond"
-                }
-    correct_output.append(temp_row)
+    # Make sure the output is correct
+    correct_data = read_json(correct_output_path)
+    output_data = read_json(output_file_path)
 
-    # Row 4
-    temp_row = {"token": 0,
-                "logfile": "ima_log.txt",
-                "vehicle": "boat boat",
-                "date_captured": "2019-07-01",
-                "location": "ocean"
-                }
-    correct_output.append(temp_row)
+    ignore_keys = ["token"]
 
-    output = get_logs("assets/example_logs")
+    assert len(correct_data) == len(output_data)
 
-    # Assert get_logs gave correct output (excluding "token")
-    for row in range(4):
-        for key in correct_output[row]:
-            if key != "token":
-                assert correct_output[row][key] == output[row][key]
+    for i in range(len(output_data)):
+        output_d = output_data[i]
+        correct_d = correct_data[i]
+        assert equal_dicts(output_d, correct_d, ignore_keys=ignore_keys)
+
+    shutil.rmtree(output_file_path)
+
 
