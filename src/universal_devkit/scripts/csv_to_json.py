87d3bb935363dad58d25b5af4328a74fac4b471d
pparfrey <pparfrey@umich.edu>
2021-03-26 14:57:11 -0400

    FEAT: Added file to convert CSV to JSON

--- src/universal_devkit/scripts/csv_to_json.py
+++ src/universal_devkit/scripts/csv_to_json.py
@@ -10,12 +10,12 @@ def main(csv_in, json_out):
         csv_in (str): the path to the CSV input file
         json_out (str): the path to save the resulting JSON file
     """
-    data_list = list(csv.DictReader(open(csv_in)))
+    data_list = list(csv.DictReader(open(csv_in), skipinitialspace = True))
 
-    # for obj in data_list:
-    #     if token not in obj:
-    #         # Avoid overwriting existing tokens
-    #         obj["token"] = uuid.uuid4().hex
+    for obj in data_list:
+        if "token" not in obj:
+            # Avoid overwriting existing tokens
+            obj["token"] = 100 # FIXXXX!!!!
 
     with open(json_out, "w") as f:
         json.dump(data_list, f)
