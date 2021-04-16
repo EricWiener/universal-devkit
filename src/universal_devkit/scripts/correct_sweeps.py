531f690997f98bc9994cedb95c74614ba0fb2cb5
Luke Lesh <leshlu@umich.edu>
2021-04-16 01:46:19 +0000

    FIX: correctly moves all images needed  to the sweeps folder

--- src/universal_devkit/scripts/correct_sweeps.py
+++ src/universal_devkit/scripts/correct_sweeps.py
@@ -23,24 +23,35 @@ def correct_sweeps(input_dir):
     camera_images = [
         int(Path(filepath).stem) for filepath in glob("{}/*.png".format(camera_dir))
     ]
-    camera_images.sort()
+
+    pcd_sweeps = []
+    pcd_samples = []
 
     for filename in os.listdir(annotation_dir):
+        filename_no_extension, extension = os.path.splitext(filename)
+        filename_no_extension_no_pcd, extension = os.path.splitext(
+            filename_no_extension
+        )
         if os.path.getsize(os.path.join(annotation_dir, filename)) == 2:
-            filename_no_extension, extension = os.path.splitext(filename)
             pcd_file = os.path.join(
                 input_dir, "samples", "LIDAR_TOP", filename_no_extension
             )
             shutil.move(pcd_file, sweeps_pcd_dir)
-            filename_no_extension_no_pcd, extension = os.path.splitext(
-                filename_no_extension
-            )
-            image = get_closest_match(camera_images, int(filename_no_extension_no_pcd))
-            image_file = os.path.join(camera_dir, str(image) + ".png")
-            shutil.move(image_file, sweeps_image_dir)
-
+            pcd_sweeps.append(int(filename_no_extension_no_pcd))
             # Remove the empty annotation file
             os.remove(os.path.join(annotation_dir, filename))
+        else:
+            pcd_samples.append(int(filename_no_extension_no_pcd))
+
+    pcd_sweeps.sort()
+    pcd_samples.sort()
+
+    for image in camera_images:
+        closest_sweep = get_closest_match(pcd_sweeps, image)
+        closest_sample = get_closest_match(pcd_samples, image)
+        if abs(closest_sweep - image) < abs(closest_sample - image):
+            image_file = os.path.join(camera_dir, str(image) + ".png")
+            shutil.move(image_file, sweeps_image_dir)
 
 
 if __name__ == "__main__":
